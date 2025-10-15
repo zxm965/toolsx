2efce21739095d15b7b9d2749d180e9644a1925c
zxm985 <ll526e@163.com>
2025-10-15 10:26:43 +0800

    chore: simplify build configuration by removing commented entries and retaining only the main index

--- build.config.ts
+++ build.config.ts
@@ -1,25 +1,4 @@
-import { resolve } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
-// export default defineBuildConfig([
-//   {
-//     entries: ['./index.ts'],
-//     declaration: true,
-//     clean: false,
-//     rootDir: resolve(__dirname, './shared'),
-//   },
-//   {
-//     entries: ['./index.ts'],
-//     declaration: true,
-//     clean: false,
-//     rootDir: resolve(__dirname, './utils'),
-//   },
-//   {
-//     entries: ['./index.ts'],
-//     declaration: true,
-//     clean: false,
-//     outDir: resolve(__dirname, './dist'),
-//   },
-// ])
 export default defineBuildConfig({
   entries: [
     './index.ts',
